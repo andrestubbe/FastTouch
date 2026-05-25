28b575d253005e339ec5a5ca499df222572b0e31
Andre Stubbe <andrestubbe@proton.me>
2026-05-25 15:17:24 +0200

    Auto Update

--- run-demo.bat
+++ run-demo.bat
@@ -1,9 +1,9 @@
 @echo off
 echo ⚡ Building Main Project...
-call mvn clean package -DskipTests
+call mvn -q clean package -DskipTests
 if %ERRORLEVEL% NEQ 0 ( pause & exit /b )
 echo 🚀 Running Hero Demo...
 cd examples\00-basic-usage
-call mvn compile exec:java -Dexec.mainClass=fasttouch.Demo
+call mvn -q compile exec:java -Dexec.mainClass=fasttouch.Demo
 cd ..\..
 pause
